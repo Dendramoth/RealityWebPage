42c1f0ccbe7192a46ef641814ee533ba4561d6a3
Dendramoth <vitsi@seznam.cz>
2018-02-02 17:42:34 +0100

    Firsts commit

---
+++ src/main/java/com/realitySiteProject/RealityWebSiteProject/RealityWebSiteProjectApplication.java
@@ -0,0 +1,12 @@
+package com.realitySiteProject.RealityWebSiteProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealityWebSiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealityWebSiteProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/realitySiteProject/RealityWebSiteProject/RealityWebSiteProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.realitySiteProject.RealityWebSiteProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealityWebSiteProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
